d4df441979d41d251aa59e0a7e8d53fba841c4af
Endrews <elijah.smiael@gmail.com>
2022-06-20 13:53:26 -0500

    add basic functionality for MVP

---
+++ controllers/flights.js
@@ -0,0 +1,106 @@
+
+import { Flight } from "../models/flight.js"
+
+function index(req, res) {
+  Flight.find({})
+  .then(flights => {
+    res.render('flights/index', {
+      title: 'All Flights',
+      flights: flights,
+    })
+  })
+  .catch(error => {
+    if (error) return res.render('flights/new')
+    res.redirect('/flights')
+  })
+}
+
+function newFlight(req, res) {
+  const newFlight = new Flight()
+  const dt = newFlight.departs
+  const departsDate = dt.toISOString().slice(0, 16)
+  res.render('flights/new', {departsDate, 
+    title: 'Add Flight',
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/')
+  })
+}
+
+function create(req, res) {
+  for (let key in req.body) {
+    if (req.body[key] === '') delete req.body[key]
+  }
+  Flight.create(req.body)
+  .then(flight => {
+    res.redirect('/flights')
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/flights/new')
+  })
+}
+
+function deleteFlight(req, res) {
+  Flight.findByIdAndDelete(req.params.id)
+  .then(() => {
+    res.redirect('/flights')
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/')
+  })
+}
+
+function show(req, res) {
+  Flight.findById(req.params.id)
+  .then(flight => {
+    res.render('flights/show', {
+      title: 'Flight Details',
+      flight: flight,
+    })
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/')
+  })
+}
+
+function edit(req, res) {
+  Flight.findById(req.params.id)
+  .then(flight => {
+    res.render('flights/edit', {
+      flight: flight,
+      title: 'Edit Flight'
+    })
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/')
+  })
+}
+
+function update(req, res) {
+  for (let key in req.body) {
+    if (req.body[key] === '') delete req.body[key]
+	}
+  Flight.findByIdAndUpdate(req.params.id, req.body, {new: true})
+  .then(flight => {
+    res.redirect(`/flights/${flight._id}`)
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/')
+  })
+}
+
+export {
+  newFlight as new,
+  create,
+  index,
+  deleteFlight as delete,
+  show,
+  edit,
+  update,
+}
---
+++ routes/flights.js
@@ -0,0 +1,28 @@
+import { Router } from 'express'
+const router = Router()
+import * as flightsCtrl from '../controllers/flights.js'
+
+// GET -- localhost:3000/flights
+router.get('/', flightsCtrl.index)
+
+// GET -- localhost:3000/flights/new
+router.get('/new', flightsCtrl.new)
+
+// GET -- localhost:3000/flights/:id
+router.get('/:id', flightsCtrl.show)
+
+// GET -- localhost:3000/flights/:id/edit
+router.get('/:id/edit', flightsCtrl.edit)
+
+// POST -- localhost:3000/flights
+router.post('/', flightsCtrl.create)
+
+// PUT -- localhost:3000/flights/:id
+router.put('/:id', flightsCtrl.update)
+
+// DELETE -- localhost:3000/flights/:id
+router.delete('/:id', flightsCtrl.delete)
+
+export {
+  router
+}
